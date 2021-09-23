483b70504d59b3b65acef78412bf447bca2c81ab
Gabriel Blanco <geblarot96@gmail.com>
2021-09-22 22:02:51 -0500

    Controller Build

--- src/com/gabensoft/controller/Controller.kt
+++ src/com/gabensoft/controller/Controller.kt
@@ -1,8 +1,33 @@
 package com.gabensoft.controller
 
+import com.gabensoft.model.VehicleContainer
+import java.util.*
+
 class Controller {
+    private val model: VehicleContainer = VehicleContainer()
+    private val sc: Scanner = Scanner(System.`in`)
+
+    fun console() {
+        var active = true
+        while (active) {
+            try {
+                print("Ingresa una opcion: ")
+                when (sc.nextLine()) {
+                    "0" -> active = false
+                    "1" -> help()
+                    else -> println("Ingresa una opción válida")
+                }
+            } catch (e: Exception) {
+                println(e.message)
+            }
+        }
+    }
+
+    fun help() {
+
+    }
 }
 
 fun main() {
-
+    Controller().console()
 }
